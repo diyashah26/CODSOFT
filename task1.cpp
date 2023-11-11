b16c07e4c9214666616ea9668258aa75bcc56cba
diyashah26 <diyashah2628@gmail.com>
2023-11-11 23:49:33 +0530

    final program

--- task1.cpp
+++ task1.cpp
@@ -1,47 +1,85 @@
-//number guessing game
+//number guessing  game
+//made by Diya Shah
+//CODSOFT
 #include<cstdlib>
 #include<iostream>
 #include<ctime>
 using namespace std;
-int main(){
+class game{
+public:
+void show(){
 srand(time(0));
-cout<<"welcome to number guessing game!\nIn this,you need to guess number (btw 1 to 100) until you reach the final one\nso let's see your guessing power"<<endl;
-int num;
+int num,i=0;
 int random=rand() % 100 + 1;
-while(num>=0){
+while(i<15){
     cout<<"enter number: ";
     cin>>num;
-if(num==random){
-    cout<<"wohoo! You have got the right number\nwe hope you enjoyed\nthanks for playing";
+    if(num==random){
+    cout<<"wohoo! You have got the right number\nwe hope you enjoyed"<<endl;
     break;
     }
     else if(num==(random/2)){
     cout<<"you have got the half of it"<<endl;
+    i++;
     }
     else if(num<(random/2)){
     cout<<"oops! too low"<<endl;
+    i++;
     }
-    else if(num>(2*random)){
+    else if(num>(random+(random/2))){
     cout<<"oops! too high"<<endl;
+    i++;
     }
-    else if((num>random && num<=(random+5)) || ((num<random && num>=(random-5)))){
-    cout<<"too close!"<<endl;
+    else if((num>random && num<=(random+5))){
+    if(num%2==0){
+            cout<<"almost there"<<endl;
+        }
+        else{
+            cout<<"too close!"<<endl;
+        }
+    i++;
     }
-    else if(num>(random/2) && num<(random-10)){
+    else if(((num<random && num>=(random-5)))){
+        if(num%2==0){
+            cout<<"almost there.."<<endl;
+        }
+        else{
+            cout<<"too close!"<<endl;
+        }
+        i++;
+    }
+    else if(num>(random/2) && num<(random-5)){
     cout<<"cool! you need to guess a higher value from the previous one"<<endl;
+    i++;
     }
-    else if(num<(2*random) && num>(random+10)){
+    else if(num<(random+(random/2)) && num>(random+5)){
     cout<<"cool! you need to guess a lower value from the previous one"<<endl;
+    i++;
     }
-    else if(num<(random-10) && num>=(random-10)){
-    cout<<"you are almost there, guess a lower value"<<endl;
-    }
-    else if(num<(random+10) && num>=(random+10)){
-    cout<<"you are almost there, guess a higher value"<<endl;
-    }    
 else{
     cout<<"wrong answer"<<endl;
+    i++;
     }
 }
-    return 0; 
+if(i>=15){
+    cout<<"sorry you are out of moves!"<<endl;
+}
+}};
+int main(){
+cout<<"---Welcome to number guessing game!---\nIn this,you need to guess number (btw 1 to 100) until you reach the final one\nyou'll get 15 chances for it\nso let's see your guessing power"<<endl;
+game g;
+g.show();
+char rep;
+while(0==0){
+cout<<"would you like to play it again?"<<endl;
+    cin>>rep;
+    if(rep=='y'){
+        cout<<"great!"<<endl;
+        g.show();
+    }
+    else{
+        cout<<"we hope you enjoyed!\nsee you soon";
+        break;
+    }}
+return 0; 
 }
--- task2.cpp
+++ task2.cpp
@@ -5,7 +5,6 @@ int main(){
     float num1,num2;
     string o;
     cout<<"SIMPLE CALCULATOR\nPlease enter the operation you want to perform\nFor addition = 'add' or '+'\nFor subtraction = 'subtract' or '-'\nFor multiplication = 'multiply' or '*'\nFor division = 'divide' or '/'"<<endl;
-    while(0==0){
     cout<<"enter first value: ";
     cin>>num1;
     cout<<"enter second value: ";
@@ -13,19 +12,19 @@ int main(){
     cout<<"operation to perform: ";
     cin>>o;
     if(o=="add" || o=="+"){
-    cout<<"sum: "<<num1+num2;
+    cout<<"sum: "<<num1+num2<<endl;
     }
     else if(o=="subtract" || o=="-"){
-    cout<<"difference: "<<num1-num2;
+    cout<<"difference: "<<num1-num2<<endl;
     }
     else if(o=="multiply" || o=="*"){
-    cout<<"product: "<<num1*num2;
+    cout<<"product: "<<num1*num2<<endl;
     }
     else if(o=="divide" || o=="/"){            
-    cout<<"quotient: "<<num1/num2;
+    cout<<"quotient: "<<num1/num2<<endl;
     }
     else{
     cout<<"wrong choice"<<endl;
-    }}
+    }
     return 0; 
 }
